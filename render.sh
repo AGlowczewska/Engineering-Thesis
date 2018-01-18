c91e24f9c2085c69b23d31d7054a811f55462eb7
Mateusz Bartos <geekbajt@gmail.com>
2018-01-18 11:26:33 +0100

    Removed eng version

--- render.sh
+++ render.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-pdflatex thesis-bachelor-english.tex 
-bibtex   thesis-bachelor-english
-pdflatex thesis-bachelor-english.tex 
-pdflatex thesis-bachelor-english.tex 
-
 pdflatex thesis-bachelor-polski.tex 
 bibtex   thesis-bachelor-polski
 pdflatex thesis-bachelor-polski.tex 
